b5d8fe6f344cf83fd28d83bb7958437c015bda2e
Ninja <srebrzuch@gmail.com>
2015-12-09 11:17:37 +0000

    add some files, add some section

--- js/new.js
+++ js/new.js
@@ -1,3 +1,10 @@
 /**
  * Created by Ninja on 2015-12-03.
  */
+$(document).ready(function () {
+    $('._f').slick('slickAdd',"<div></div>");
+
+// Get the current slide
+    var currentSlide = $('._f').slick('._f');
+
+});
