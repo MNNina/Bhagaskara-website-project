4828f79f4642acc080cf67de49af1dd50dcd5a8a
Ninja <srebrzuch@gmail.com>
2015-12-10 16:43:55 +0000

    add contact, add items

--- js/new.js
+++ js/new.js
@@ -2,9 +2,28 @@
  * Created by Ninja on 2015-12-03.
  */
 $(document).ready(function () {
-    $('._f').slick('slickAdd',"<div></div>");
+    //$('._f').slick('slickAdd',"<div></div>");
 
 // Get the current slide
-    var currentSlide = $('._f').slick('._f');
+    //var currentSlide = $('._f').slick('._f');
 
+
+
+
+
+    var navi = $('#nav1').offset().top;
+    $(window).on('scroll', function(){
+        if ($(window).scrollTop() > navi - 10) {
+            $('#nav').addClass('fixed');
+        } else {
+            $('#nav').removeClass('fixed');
+        }
+
+        if ($(window).scrollTop() > navi) {
+            $('#nav').addClass('fixed-change');
+        } else {
+            $('#nav').removeClass('fixed-change');
+        }
+
+    });
 });
